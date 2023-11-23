9701bd7571c066098a84125bf92e97f5e289be62
cfgm1603 <cesargimo@unisabana.edu.co>
2023-11-22 20:14:03 -0500

    Added pages and fixed bugs

--- src/components/CreateAppointmentComponent.jsx
+++ src/components/CreateAppointmentComponent.jsx
@@ -1,22 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class CreateAppointmentComponent extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {};    
-    }
+    this.state = {
+      task: "Active",
+      customer: "",
+      employee: "",
+      price: "",
+      type: "",
+    };
+  }
+
+  changeTypeHandlerTask = (event) => {
+    this.setState({type: event.target.value});
+  }
+
+  changeTypeHandlerEmployer = (event) => {
+    this.setState({type: event.target.value});
+  }
   render() {
-    return (
-      <div> 
-        <h1> Appointment Form</h1>
-      
-      
-      
-      
-      
-      
-       </div>
+    return(
+      <div>
+            <div className="container">
+
+                <div className="row">
+                    <div className="card col-md-6 offset-md-3 offset-md-3">
+                      <h3 className="text-center">Make an appointment</h3>
+                      <div className="card-body">
+                          <form>
+                              <div className="form-group">
+                                  <label>Appointment task ID</label>
+                                  <input placeholder="Appointment type ID" name="task" className="form-control" value={this.state.task} onChange={this.changeTypeHandlerTask}/>
+                              </div>
+                              <div className="form-group">
+                                  <label>Employeer ID</label>
+                                  <input placeholder="Employeer ID" name="customer" className="form-control" value={this.state.task} onChange={this.changeTypeHandlerEmployer}/>
+                              </div>
+                              <div className="form-group">
+                                  <label>Date (with the "-")</label>
+                                  <input placeholder="yyyy-mm-dd" name="employee" className="form-control"/>
+                              </div>
+                              <div className="form-group">
+                                  <label>Price</label>
+                                  <input placeholder="Price" name="price" className="form-control"/>
+                              </div>
+                              <button className="btn btn-success" >Save</button>
+                              <button className="btn btn-danger" >Cancel</button>
+                          </form> 
+
+
+                    </div>
+                </div>
+                
+                </div>
+            </div>
+      </div>
     );
   }
 }
--- src/components/ListAppointmentComponent.jsx
+++ src/components/ListAppointmentComponent.jsx
@@ -1,38 +1,39 @@
-import React, { Component } from 'react';
-import { useNavigate} from 'react-router-dom';
-import EmployeeService from '../services/AppointmentService';
-import AppointmentService from '../services/AppointmentService';
+import React, { Component } from "react";
+import { useNavigate } from "react-router-dom";
+import EmployeeService from "../services/AppointmentService";
+import AppointmentService from "../services/AppointmentService";
+import { Link } from "react-router-dom";
 
 class ListAppointmentComponent extends Component {
-
   constructor(props) {
     super(props);
+
+    //Aca se crea un state, estos son como objetos que viven dentro de React y se pueden modificar mediante setters
     this.state = {
       appointments: [],
     };
-    
   }
 
+  //Aca se crea un metodo que se ejecuta cuando se carga el componente, meaning que si se actualizan datos, el actualiza el componente (el front)
   componentDidMount() {
     AppointmentService.getAppointments().then((res) => {
-      this.setState({appointments: res.data});
+      this.setState({ appointments: res.data });
     });
 
+    //ignorar solo es para poder probar POST en la api
     AppointmentService.createAppointment();
   }
-
- 
 
   render() {
     return (
       <div>
         <h2 className="text-center">Appointment List</h2>
-        <div className='row'>
-          {/* <button className="btn btn-primary" onClick={this.addEmployee}>Add Employee</button> */}
-        </div>
+        
+        {/* Aca se crea una tabla */}
         <div className="row">
           <table className="table table-striped table-bordered">
             <thead>
+              {/* //campos de la tabla */}
               <tr>
                 <th>Appointment type</th>
                 <th>Client Name</th>
@@ -41,21 +42,24 @@ class ListAppointmentComponent extends Component {
                 <th>Actions</th>
               </tr>
             </thead>
-
+            {/* aca se llena la tabla con la data de la api */}
             <tbody>
-              {
-                this.state.appointments.map(
-                  appointment =>
-                    <tr key={appointment.appointmentId}>
-                      <td>{appointment.task.name}</td>
-                      <td>{appointment.customer.name}</td>
-                      <td>{appointment.employee.name}</td>
-                      <td>{appointment.task.price}</td>
-                    </tr>
-                )
-              }
+              {this.state.appointments.map((appointment) => (
+                <tr key={appointment.appointmentId}>
+                  {/* fijarse que si el dato estaba dentro de otro objeto toca llamarlo */}
+                  <td>{appointment.task.name}</td>
+                  <td>{appointment.customer.name}</td>
+                  <td>{appointment.employee.name}</td>
+                  <td>{appointment.task.price}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
+          <div>
+          <Link to="/add-appointment">
+            <button className="btn btn-primary">Make an appointment</button>
+          </Link>
+        </div>
         </div>
       </div>
     );
